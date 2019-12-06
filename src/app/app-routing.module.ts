323e2e6e25f4471f176487a805264cdf5b314865
Abdelrahman Mahmoud <Abdelrahman.m.fcih@gmail.com>
2019-12-06 21:22:34 +0200

    Add stack and queue

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,28 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { StackComponent } from './pages/stack/stack.component';
+import { QueueComponent } from './pages/queue/queue.component';
+import { PriorityQueueComponent } from './pages/priority-queue/priority-queue.component';
+import { SinglyListComponent } from './pages/singly-list/singly-list.component';
+import { DoublyListComponent } from './pages/doubly-list/doubly-list.component';
+import { HashTableComponent } from './pages/hash-table/hash-table.component';
+import { BSTComponent } from './pages/bst/bst.component';
+import { GraphComponent } from './pages/graph/graph.component';
+import { MaxBinaryHeapComponent } from './pages/max-binary-heap/max-binary-heap.component';
+
+const routes: Routes = [
+  {path:'', redirectTo:'stack',pathMatch:'full'},
+  {path:'stack',component:StackComponent},
+  {path:'queue',component:QueueComponent},
+  {path:'priority-queue',component:PriorityQueueComponent},
+  {path:'singly-list',component:SinglyListComponent},
+  {path:'doubly-list',component:DoublyListComponent},
+  {path:'hash-table',component:HashTableComponent},
+  {path:'bst',component:BSTComponent},
+  {path:'graph',component:GraphComponent},
+  {path:'max-binary-heap',component:MaxBinaryHeapComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,17 +2,60 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
+/*Components to be declared */
 import { AppComponent } from './app.component';
+import { NavComponent } from './partials/nav/nav.component';
+import { StackComponent } from './pages/stack/stack.component';
+import { QueueComponent } from './pages/queue/queue.component';
+import { PriorityQueueComponent } from './pages/priority-queue/priority-queue.component';
+import { SinglyListComponent } from './pages/singly-list/singly-list.component';
+import { DoublyListComponent } from './pages/doubly-list/doubly-list.component';
+import { HashTableComponent } from './pages/hash-table/hash-table.component';
+import { BSTComponent } from './pages/bst/bst.component';
+import { GraphComponent } from './pages/graph/graph.component';
+import { MaxBinaryHeapComponent } from './pages/max-binary-heap/max-binary-heap.component';
+/*Classes to be provided */
+import { Stack } from './DS-models/stack';
+import { StackArr } from './DS-models/stack-arr';
+import { Queue } from './DS-models/queue';
+import { QueueArr } from './DS-models/queue-arr';
+import { PriorityQueue } from './DS-models/priority-queue';
+import { DouplyLinkedList } from './DS-models/douply-linked-list';
+import { SinglyLinkedList } from './DS-models/singly-linked-list';
+import { HashTable } from './DS-models/hash-table';
+import { BinarySearchTree } from './DS-models/binary-search-tree';
+import { MaxBinaryHeap } from './DS-models/max-binary-heap';
+import { Graph } from './DS-models/graph';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent,
+    StackComponent,
+    QueueComponent,
+    PriorityQueueComponent,
+    SinglyListComponent,
+    DoublyListComponent,
+    HashTableComponent,
+    BSTComponent,
+    GraphComponent,
+    MaxBinaryHeapComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [Stack,
+              StackArr,
+              Queue,
+              QueueArr,
+              PriorityQueue,
+              DouplyLinkedList,
+              SinglyLinkedList,
+              HashTable,
+              BinarySearchTree,
+              MaxBinaryHeap,
+              Graph],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
